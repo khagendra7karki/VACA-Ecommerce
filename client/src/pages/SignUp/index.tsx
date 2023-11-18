91744c795d4e84defbe40a02da6bd9b0783a00d2
Divya <karkidivya5@gmail.com>
2023-11-18 12:57:35 +0545

    signup-css-worked

--- client/src/pages/SignUp/index.tsx
+++ client/src/pages/SignUp/index.tsx
@@ -1,96 +1,124 @@
 import {
-    Paper,
-    TextInput,
-    PasswordInput,
-    Checkbox,
-    Button,
-    Title,
-    Text,
-    Anchor,
-  } from '@mantine/core';
-import classes from './Signup.module.css';
-import { useNavigate } from 'react-router-dom';
-import { useForm } from '@mantine/form';
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from '../../state'
-  
+  Paper,
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Button,
+  Title,
+  Text,
+  Anchor,
+  Grid,
+  Image,
+  Container,
+  Space,
+  Divider,
+} from "@mantine/core";
+import img1 from "../../assets/img/img2.jpg";
+import classes from "./Signup.module.css";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "@mantine/form";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../../state";
 interface credential {
-  email: string,
-  password: string,
-  fullName: string,
+  email: string;
+  password: string;
+  fullName: string;
 }
 export function Signup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-
-  const { register } = bindActionCreators( actionCreators , dispatch )
+  const { register } = bindActionCreators(actionCreators, dispatch);
 
   const form = useForm({
     validateInputOnChange: true,
     initialValues: {
       email: "",
-      password:"",
+      password: "",
       fullName: "",
-
-    }
-  })
-  const handleSubmit = (values: credential ) =>{
-    register( values.fullName, values.email, values.password )
+    },
+  });
+  const handleSubmit = (values: credential) => {
+    register(values.fullName, values.email, values.password);
 
     // console.log(`A user has been register \n`, values)
-  }
+  };
 
   return (
-    <form onSubmit = { form.onSubmit(( values ) => { handleSubmit( values )})}>
-      <div className={classes.wrapper}>
-        <Paper className={classes.form} radius={0} p={30}>
-          <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
-            Welcome back to Mantine!
-          </Title>
+    <Container fluid h={800} bg="var(--mantine-color-blue-light)" pl={0} ml={0} mb = {50}>
+      <Grid gutter={{ base: 5, xs: 'md', md: 'xl', xl: 100 }}>
+        <Grid.Col span={{ base: 12, md: 12, lg: 8 }}>
+          <Image  src={img1} h={781} w = 'auto' fit="contain" />
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 12, lg: 4 }} style = {{display:'flex', justifyContent  :' center' , alignItems :'center'}}>
+          <form
+            onSubmit={form.onSubmit((values) => {
+              handleSubmit(values);
+            })}
+          >
+            <div className={classes.wrapper}>
+              <Paper className={classes.form} radius={0} p={30}>
+                <Title
+                  order={2}
+                  className={classes.title}
+                  ta="left"
+                  mt="md"
+                  mb={5}
+                >
+                Create an account
+                </Title>
+                <Text size="xs">Enter your details below</Text>
+                <Space h= 'xl'></Space>
 
-          <TextInput 
-            label="Email address" 
-            placeholder="Ram Sharma" 
-            size="md"
-            {...form.getInputProps("fullName")}
-            />
-          
-          <TextInput 
-            label="Email address" 
-            placeholder="hello@gmail.com" 
-            size="md"
-            {...form.getInputProps("email")}
-            />
-          
-          <PasswordInput 
-            label="Password" 
-            placeholder="Your password" 
-            mt="md"
-            size="md"
-            {...form.getInputProps("password")} 
-            />
-          
-          <Checkbox label="Keep me logged in" mt="xl" size="md" />
-          <Button 
-            fullWidth mt="xl" 
-            size="md"
-            type = 'submit'
-            >
-                    
-            Register
-          </Button>
+                <TextInput
+                variant="unstyled"
+                 // label="Email address"
+                  placeholder="Name"
+                  size="md"
+                  {...form.getInputProps("fullName")}
+                />
+                <Divider my="sm" />
 
-          <Text ta="center" mt="md">
-            Alread have an account?{' '}
-            <Anchor<'a'> href="#" fw={700} onClick={(event) => navigate('/login')}>
-              Login
-            </Anchor>
-          </Text>
-        </Paper>
-      </div>
-    </form>
+                <TextInput
+                variant="unstyled"
+                 // label="Email address"
+                  placeholder="Email or Phone Number"
+                  size="md"
+                  {...form.getInputProps("email")}
+                />
+ <Divider my="sm" />
 
+                <PasswordInput
+                variant="unstyled"
+                 // label="Password"
+                  placeholder="Password"
+                  mt="md"
+                  size="md"
+                  {...form.getInputProps("password")}
+                />
+                <Divider my="sm" />
+
+                {/* <Checkbox label="Keep me logged in" mt="xl" size="md" /> */}
+                <Button fullWidth mt="xl" size="md" type="submit" color = '#DB4444'>
+                  Register
+                </Button>
+
+                <Text ta="center" mt="md">
+                  Alread have an account?{" "}
+                  <Anchor<"a">
+                    href="#"
+                    fw={700}
+                    onClick={(event) => navigate("/login")}
+                  >
+                    Login
+                  </Anchor>
+                </Text>
+              </Paper>
+            </div>
+          </form>
+        </Grid.Col>
+      </Grid>
+    </Container>
   );
 }
