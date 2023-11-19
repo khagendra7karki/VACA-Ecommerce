7ce6d2ed39529a2e9f872f3f0b9b4d9bb3670492
Divya <karkidivya5@gmail.com>
2023-11-19 11:26:40 +0545

    footer-css

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,34 +1,50 @@
-import { Text, Container, ActionIcon, Group, rem, Space, SimpleGrid } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons-react';
-import classes from './Footer.module.css';
-import { v4 as uuidv4 } from 'uuid';
+import {
+  Text,
+  Container,
+  ActionIcon,
+  Group,
+  rem,
+  Space,
+  SimpleGrid,
+  Title,
+  TextInput,
+  Divider,
+  Grid,
+} from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import classes from "./Footer.module.css";
+import { v4 as uuidv4 } from "uuid";
 
 const data = [
   {
-    title: 'About',
+    title: "About",
     links: [
-      { label: 'Features', link: '#' },
-      { label: 'Pricing', link: '#' },
-      { label: 'Support', link: '#' },
-      { label: 'Forums', link: '#' },
+      { label: "Features", link: "#" },
+      { label: "Pricing", link: "#" },
+      { label: "Support", link: "#" },
+      { label: "Forums", link: "#" },
     ],
   },
   {
-    title: 'Project',
+    title: "Project",
     links: [
-      { label: 'Contribute', link: '#' },
-      { label: 'Media assets', link: '#' },
-      { label: 'Changelog', link: '#' },
-      { label: 'Releases', link: '#' },
+      { label: "Contribute", link: "#" },
+      { label: "Media assets", link: "#" },
+      { label: "Changelog", link: "#" },
+      { label: "Releases", link: "#" },
     ],
   },
   {
-    title: 'Community',
+    title: "Community",
     links: [
-      { label: 'Join Discord', link: '#' },
-      { label: 'Follow on Twitter', link: '#' },
-      { label: 'Email newsletter', link: '#' },
-      { label: 'GitHub discussions', link: '#' },
+      { label: "Join Discord", link: "#" },
+      { label: "Follow on Twitter", link: "#" },
+      { label: "Email newsletter", link: "#" },
+      { label: "GitHub discussions", link: "#" },
     ],
   },
 ];
@@ -36,22 +52,25 @@ const data = [
 export function Footer() {
   const groups = data.map((group) => {
     const links = group.links.map((link, index) => (
-      <><Text<'a'>
-        key={uuidv4()}
-        className={classes.link}
-        component="a"
-        href={link.link}
-        onClick={(event) => event.preventDefault()}
-      >
-        {link.label}
-      </Text>
-       <Space h="sm" /></>
+      <>
+        <Text<"a">
+          key={uuidv4()}
+          className={classes.link}
+          component="a"
+          href={link.link}
+          onClick={(event) => event.preventDefault()}
+        >
+          {link.label}
+        </Text>
+        <Space h="sm" />
+      </>
     ));
 
     return (
       <div className={classes.wrapper} key={uuidv4()}>
         <SimpleGrid cols={3} spacing="xl">
-        <Text className={classes.title}>{group.title}</Text></SimpleGrid>
+          <Text className={classes.title}>{group.title}</Text>
+        </SimpleGrid>
         {links}
       </div>
     );
@@ -60,31 +79,78 @@ export function Footer() {
   return (
     <footer className={classes.footer}>
       <Container className={classes.inner} fluid>
-        <div className={classes.logo}>
-          {/* <MantineLogo size={30} /> */}
-          <Text size="xs" c="dimmed" className={classes.description}>
-            Shop Bindas Shop Cool
-          </Text>
-        </div>
-        <div className={classes.groups}>{groups}</div>
+        <Grid>
+          <Grid.Col span={3}>
+            <div className={classes.logo}>
+              {/* <MantineLogo size={30} /> */}
+              <Title order={2} c="white">
+                VACA
+              </Title>
+              <Space h="md" />
+              <Text size="lg" c="white" className={classes.description}>
+                Subscribe
+              </Text>
+              <Text size="sm" c="white" className={classes.description}>
+                Get 10% off your first order
+              </Text>
+              <TextInput placeholder="Enter your email" mt="15px" style={{ width: rem(300) }}/>
+            </div>
+          </Grid.Col>
+          <Grid.Col span={6} >
+            {" "}
+            <div className={classes.groups}>{groups}</div>
+          </Grid.Col>
+          <Grid.Col span={3}>
+            {" "}
+            <div>
+              <Title order={2} c="white">
+                Connect With Us
+              </Title>
+              <Space h="md" />
+              <Text size="lg" c="white" className={classes.description}>
+                Vaca here,
+              </Text>
+              <Text size="sm" c="white" className={classes.description}>
+                TakeFree
+              </Text>
+              <Container className={classes.afterFooter}>
+                <Group
+                  gap={0}
+                  className={classes.social}
+                  justify="flex-end"
+                  wrap="nowrap"
+                >
+                  <ActionIcon size="lg" color="gray" variant="subtle">
+                    <IconBrandTwitter
+                      style={{ width: rem(18), height: rem(18) }}
+                      stroke={1.5}
+                    />
+                  </ActionIcon>
+                  <ActionIcon size="lg" color="gray" variant="subtle">
+                    <IconBrandYoutube
+                      style={{ width: rem(18), height: rem(18) }}
+                      stroke={1.5}
+                    />
+                  </ActionIcon>
+                  <ActionIcon size="lg" color="gray" variant="subtle">
+                    <IconBrandInstagram
+                      style={{ width: rem(18), height: rem(18) }}
+                      stroke={1.5}
+                    />
+                  </ActionIcon>
+                </Group>
+              </Container>
+            </div>
+          </Grid.Col>
+        </Grid>
       </Container>
-      <Container className={classes.afterFooter}>
-        <Text c="dimmed" size="sm">
+
+      <Divider color="white " />
+      <Group justify="center">
+        <Text c="dimmed" size="sm" ta="center" mt="20px" mb="10px">
           © 2023 vaca.dev. All rights reserved.
         </Text>
-
-        <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon size="lg" color="gray" variant="subtle">
-            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-          </ActionIcon>
-        </Group>
-      </Container>
+      </Group>
     </footer>
   );
 }
