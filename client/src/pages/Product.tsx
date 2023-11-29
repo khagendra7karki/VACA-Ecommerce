958c8acd70065b191e3d6bed5bd1917cfa1851db
Divya <karkidivya5@gmail.com>
2023-11-29 20:40:45 +0545

    productPageV3

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -18,8 +18,8 @@ import {
   Text,
 } from "@mantine/core";
 import classes from "./product.module.css";
-import img1 from "../assets/img/img1.jpg"
-import img2 from "../assets/img/img2.jpg"
+import img1 from "../assets/img/img1.jpg";
+import img2 from "../assets/img/img2.jpg";
 
 import { useParams } from "react-router";
 import { bindActionCreators } from "redux";
@@ -53,206 +53,197 @@ const Product = () => {
 
   useEffect(() => {
     getProduct(params.id as string);
-    
   }, [dispatch, params]);
 
-
   // const productImages : string[] = [product.images[0], img1 , img2 , product.images[0] , img2 , img1]
-  const productImages : string[] = product.images
-  const [selectedImage, setSelectedImage] = useState<string | null>( null );
-  
+  const productImages: string[] = product.images;
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   const handleImageClick = (imageName: string) => {
     setSelectedImage(imageName);
   };
- 
+
   function productSideCarousel() {
     return (
       <Carousel
+      px={35}
         className={classes.carousel}
-        
-        slideSize={{ base: '33.333333%', sm: '25%', md: '25%' }}
-        slideGap={{ base: 0, sm: 'md' }}
-        controlsOffset="sm"
-        //loop
+        height='100%'
+        slideSize={{ base: "33.333333%", sm: "25%", md: "20%" }}
+        slideGap={{ base: 0, sm: "0" , xl :'0' }}
+        controlsOffset="xs"
         align="start"
-        //orientation="vertical"
-        
       >
         {productImages.map((image) => (
-          
-            <Carousel.Slide mt={5}>
-              <Image src = {image}  fit="contain" onClick={() => handleImageClick(image)} /></Carousel.Slide>
-        
+          <Carousel.Slide >
+            <Image
+              src={image}
+              h={100}
+              fit="contain"
+              onClick={() => handleImageClick(image)}
+            />
+          </Carousel.Slide>
         ))}
-        
-        
       </Carousel>
     );
   }
 
-  
-  const pills = product.size?.map(( size : string, index : number ) => (
-      <Pill key={index} withRemoveButton>
-        size {size}
-      </Pill>
-    ));
+  const pills = product.size?.map((size: string, index: number) => (
+    <Pill key={index} withRemoveButton>
+      size {size}
+    </Pill>
+  ));
 
-  return (<>
-    {loading ? (
-      <Loading />
-    ) : (
-      <><Container size = '80%' >
-        {Object.keys(product).length && (<>
-          <Grid mb={100} gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}>
-            <Grid.Col span={{ base: 12, md: 4, lg: 2 }}>
-              <Group justify="center">
-                {productSideCarousel()}
-              </Group>
-            </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
-              <Group justify="center">
-                <Image
-                  radius="md"
-                  h={500}
-                  //w="auto"
-                  fit="contain"
-                  src={ selectedImage || product.images[0] }
-                  alt="product image"
-                />
-              </Group>
-            </Grid.Col>
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Container size= "100%"> 
+            {Object.keys(product).length && (
+              <>
+                <Grid mb={50} gutter={{ base: 5, xs: "md", md: "xl", xl: 50 }}>
+                  <Grid.Col span={{ base: 12, md: 4, lg: 6 }}>
+                    <Stack >
+                      <Group justify="center">
+                        <Image
+                          radius="md"
+                          h={500}
+                          //w="auto"
+                          fit="contain"
+                          src={selectedImage || product.images[0]}
+                          alt="product image"
+                        />
+                      </Group>
+                      <Group  justify="center">
+                        <div style={{width:"500px"}}>{productSideCarousel()}</div></Group>
+                    </Stack>
+                  </Grid.Col>
 
-            <Grid.Col span={{ base: 12, md: 8, lg: 6 }}>
-              <Stack
-                ml ="50px"
-                justify="flex-start"
-              >
-                <Group gap="lg">
-                  {" "}
-                  <Text fw={400} style={{ fontSize: "32px" }}>
-                    {product.title}
-                  </Text>
-                  {product.availableQuantity === 0 ? (
-                    <Badge
-                      color="red"
-                      style={{ marginLeft: "10px" }}
-                      variant="filled"
-                    >
-                      {" "}
-                      Sold Out
-                    </Badge>
-                  ) : (
-                    <Badge
-                      color="green"
-                      style={{ marginLeft: "10px" }}
-                      variant="filled"
-                    >
-                      {" "}
-                      In Stock
-                    </Badge>
-                  )}
-                </Group>
-                <Space h="xs" />
-                <Group gap="lg">
-                  <Text>Rs.  {product.price}</Text>
-                  <Badge variant="outline">25% off</Badge>
-                </Group>
-                <Divider my="sm" />
-                <Group gap="lg">
-                  <Text>Option</Text>
-                  <Select
-                    placeholder="Choose an Option"
-                    data={ product.options }
-                  />
-                </Group>
-                <Group>
-                  {" "}
-                  <InputBase component="div" multiline>
-                    <Pill.Group>{pills}</Pill.Group>
-                  </InputBase>
-                </Group>
-                <Divider my="sm" />
+                  <Grid.Col span={{ base: 12, md: 8, lg: 6 }}>
+                    <Stack  justify="flex-start">
+                      <Group gap="lg">
+                        {" "}
+                        <Text fw={400} style={{ fontSize: "32px" }}>
+                          {product.title}
+                        </Text>
+                        {product.availableQuantity === 0 ? (
+                          <Badge
+                            color="red"
+                            style={{ marginLeft: "10px" }}
+                            variant="filled"
+                          >
+                            {" "}
+                            Sold Out
+                          </Badge>
+                        ) : (
+                          <Badge
+                            color="green"
+                            style={{ marginLeft: "10px" }}
+                            variant="filled"
+                          >
+                            {" "}
+                            In Stock
+                          </Badge>
+                        )}
+                      </Group>
+                      <Space h="xs" />
+                      <Group gap="lg">
+                        <Text>Rs. {product.price}</Text>
+                        <Badge variant="outline">25% off</Badge>
+                      </Group>
+                      <Divider my="sm" />
+                      <Group gap="lg">
+                        <Text>Option</Text>
+                        <Select
+                          placeholder="Choose an Option"
+                          data={product.options}
+                        />
+                      </Group>
+                      <Group>
+                        {" "}
+                        <InputBase component="div" multiline>
+                          <Pill.Group>{pills}</Pill.Group>
+                        </InputBase>
+                      </Group>
+                      <Divider my="sm" />
 
-                <Group gap={30}>
-                  <Group>
-                    <ActionIcon
-                      size={28}
-                      radius="lg"
-                      variant="filled"
-                      color="dark"
-                      onClick={() => handlers?.current?.decrement()}
-                    >
-                      –
-                    </ActionIcon>
-                    <NumberInput
-                      hideControls
-                      value={value}
-                      onChange={(val) => setValue(val)}
-                      handlersRef={handlers}
-                      max={10}
-                      min={1}
-                      step={1}
-                      styles={{
-                        input: { width: 54, textAlign: "center" },
-                      }}
-                      radius="lg"
-                    />
-                    <ActionIcon
-                      size={28}
-                      radius="lg"
-                      variant="filled"
-                      color="dark"
-                      onClick={() => handlers?.current?.increment()}
-                    >
-                      +
-                    </ActionIcon>
-                  </Group>
+                      <Group gap={30}>
+                        <Group>
+                          <ActionIcon
+                            size={28}
+                            radius="lg"
+                            variant="filled"
+                            color="dark"
+                            onClick={() => handlers?.current?.decrement()}
+                          >
+                            –
+                          </ActionIcon>
+                          <NumberInput
+                            hideControls
+                            value={value}
+                            onChange={(val) => setValue(val)}
+                            handlersRef={handlers}
+                            max={10}
+                            min={1}
+                            step={1}
+                            styles={{
+                              input: { width: 54, textAlign: "center" },
+                            }}
+                            radius="lg"
+                          />
+                          <ActionIcon
+                            size={28}
+                            radius="lg"
+                            variant="filled"
+                            color="dark"
+                            onClick={() => handlers?.current?.increment()}
+                          >
+                            +
+                          </ActionIcon>
+                        </Group>
 
-                  <Button
-                    variant="outline"
-                    color="rgba(0, 0, 0, 0.71)"
-                    //style={{ flex: 1 }}
-                    onClick={() =>
-                      handlerAddToCart(value, product._id)
-                    }
-                  >
-                    Add To Cart
-                  </Button>
-                </Group>
-                {/* <Group>
+                        <Button
+                          variant="outline"
+                          color="rgba(0, 0, 0, 0.71)"
+                          //style={{ flex: 1 }}
+                          onClick={() => handlerAddToCart(value, product._id)}
+                        >
+                          Add To Cart
+                        </Button>
+                      </Group>
+                      {/* <Group>
                   <Button variant="filled">Add to Wishlist</Button>
                 </Group> */}
 
-                <Text fz="sm" c="dimmed" className={classes.label}>
-                  Description
-                </Text>
+                      <Text fz="sm" c="dimmed" className={classes.label}>
+                        Description
+                      </Text>
+
+                      <Group>
+                        <Text size="md" c="dimmed">
+                          {product.description}
+                        </Text>
+                      </Group>
+                    </Stack>
+                  </Grid.Col>
+                </Grid>
 
                 <Group>
-                  <Text size="md" c="dimmed">
-                    {product.description}
-                  </Text>
+                  <Reactangle />
+                  <h5 className="sub-title">Related Product</h5>
                 </Group>
-              </Stack>
-            </Grid.Col>
-          </Grid>
-
-          <Group >
-            <Reactangle />
-            <h5 className = 'sub-title'>Related Product</h5>
-            
-          </Group>
-          <CardCarousel />
-        </>
-          
-         
-        )}
-        {/* {Object.keys(product).length && (
+                <CardCarousel />
+              </>
+            )}
+            {/* {Object.keys(product).length && (
           <Review reviewM={product.reviews} />
         )} */}
-         </Container>
-      </>
-    )}
-  </>
+          </Container>
+        </>
+      )}
+    </>
   );
 };
 
