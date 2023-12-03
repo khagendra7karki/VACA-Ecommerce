d319315e0c5933e3ed9b94a2ec4a5670c6f4e5d0
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2023-12-03 12:33:30 +0545

    refactored

--- server/index.js
+++ server/index.js
@@ -3,14 +3,9 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import Database from './utils/db.js'
 
-import { fileURLToPath } from 'url';
-import path from 'path';
 //router object
 import mainRouter from './routes/index.js'
 dotenv.config()
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const db = new Database();
 const URI = process.env.MONGODB_URI
@@ -25,13 +20,10 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/', ( req, res ) =>{
-res.sendFile( path.join(__dirname, 'build', 'index.html'))
-})
+app.use('/api', mainRouter)
 
-app.use('/', mainRouter)
+
 
 app.listen( PORT, () => {
     console.log( `Server running on http://localhost:${PORT}` )
